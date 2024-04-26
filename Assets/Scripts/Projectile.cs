47089767741e188428d201f868b8058a32fafd2c
madzwo <madzwo@iu.edu>
2024-04-26 19:52:32 -0400

    bullet explode particle system

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public GameObject bulletExplode;
+
     public void OnTriggerEnter(Collider collision)
     {
         if(collision.tag != "player")
         {
+            Instantiate(bulletExplode, transform.position, transform.rotation);
             Destroy(gameObject);
         }
     }
