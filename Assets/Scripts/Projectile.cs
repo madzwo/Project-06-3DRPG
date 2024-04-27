baed77fa6668cde1ef88e3028f94bdec39f0aea4
madzwo <madzwo@iu.edu>
2024-04-27 15:36:54 -0400

    enemies die and explode

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public GameObject bulletExplode;
+    public GameObject enemyExplode;
 
     public void OnTriggerEnter(Collider collision)
     {
-        if(collision.tag == "enemy" || collision.tag == "obstacle")
+        if(collision.tag == "obstacle")
         {
             Instantiate(bulletExplode, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        if(collision.tag == "enemy")
+        {
+            Instantiate(bulletExplode, transform.position, transform.rotation);
+            Instantiate(enemyExplode, transform.position, transform.rotation);
+            Destroy(collision.gameObject);
             Destroy(gameObject);
         }
     }
